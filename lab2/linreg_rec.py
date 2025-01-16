117a51ebb8f386a4af41218e6424004a99d8a402
notpetrusha <vika04vvp@mail.ru>
2025-01-17 01:07:38 +0300

    add linreg and svd

--- lab2/linreg_rec.py
+++ lab2/linreg_rec.py
@@ -10,15 +10,40 @@ from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 
 nltk.download("stopwords")
 nltk.download("punkt")
+nltk.download('punkt_tab')
 
-
-def books_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
+def books_preprocessing(books: pd.DataFrame) -> pd.DataFrame:
     """Функция для предобработки таблицы Books.scv"""
 
-    pass
+    for i in books[books["Year-Of-Publication"].map(str).str.match("[^0-9]")]['Book-Title'].index: #убирает сдвиг в данных 
+        stroka = books.iloc[i]
+        joined_cell = stroka['Book-Title']
+        book_author = joined_cell[joined_cell.find(';')+1:-1]
+        book_title = joined_cell[:joined_cell.find("\\")]
+        year = stroka['Book-Author']
+        col_names = list(books)
+        for col_ind in range(len(col_names)):
+            if col_names[col_ind] == 'ISBN':
+                continue
+            if col_names[col_ind] == 'Book-Title':
+                books.loc[i, col_names[col_ind]] = book_title
+            elif col_names[col_ind] =='Book-Author':
+                books.loc[i, col_names[col_ind]] = book_author
+            elif col_names[col_ind] == 'Year-Of-Publication':
+                books.loc[i, col_names[col_ind]] = year
+            else:
+                books.loc[i, col_names[col_ind]] = books.iloc[i][col_names[col_ind-1]]
+
+    books['Year-Of-Publication'] = books['Year-Of-Publication'].astype(int)
+    books = books[books['Year-Of-Publication']<=2024]
+    books = books[col_names[:-3]]
+    books = books.dropna()
+
+    return books
 
 
 def ratings_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
@@ -28,7 +53,16 @@ def ratings_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
     1. Замену оценки книги пользователем на среднюю оценку книги всеми пользователями.
     2. Расчет числа оценок для каждой книги (опционально)."""
 
-    pass
+    avg_ratings = df.groupby('ISBN')['Book-Rating'].mean().reset_index()
+    avg_ratings.rename(columns={'Book-Rating': 'Avg-Rating'}, inplace=True)
+
+    df = df.merge(avg_ratings, on='ISBN', how='left')
+    
+
+    df.drop(columns=['Book-Rating', 'User-ID'], inplace=True)
+    df = df.drop_duplicates(subset=None, keep='first', inplace=False)
+
+    return df
 
 
 def title_preprocessing(text: str) -> str:
@@ -38,8 +72,11 @@ def title_preprocessing(text: str) -> str:
     - удаление пунктуации
     Опционально можно убрать шаги или добавить дополнительные.
     """
+    stop_words = set(stopwords.words("english"))
+    tokens = nltk.word_tokenize(text.lower())
+    tokens = [word for word in tokens if (word.isalpha() or word.isdigit()) and word not in stop_words]
 
-    pass
+    return " ".join(tokens)
 
 
 def modeling(books: pd.DataFrame, ratings: pd.DataFrame) -> None:
@@ -51,9 +88,45 @@ def modeling(books: pd.DataFrame, ratings: pd.DataFrame) -> None:
     5. Обучить и протестировать SGDRegressor
     6. Подобрать гиперпараметры (при необходимости)
     7. Сохранить модель"""
+    books = books_preprocessing(books)
+    ratings = ratings_preprocessing(ratings)
+    books['Book-Title'] = books['Book-Title'].apply(title_preprocessing)
+    books_with_ratings = pd.merge(books, ratings, on='ISBN', how='left')
+    books_with_ratings = books_with_ratings.dropna()
+    
+    lab_enc_author = LabelEncoder()
+    lab_enc_publisher = LabelEncoder()
 
-    # ...
-    linreg = SGDRegressor()
-    # ...
-    with open("linreg.pkl", "wb") as file:
+    books_with_ratings['Book-Author'] = lab_enc_author.fit_transform(books_with_ratings['Book-Author'])
+    books_with_ratings['Publisher'] = lab_enc_publisher.fit_transform(books_with_ratings['Publisher'])
+
+    books_with_ratings = books_with_ratings.drop(columns=['ISBN'])
+
+    vectorizer = TfidfVectorizer(max_features=1000)
+    X_title = vectorizer.fit_transform(books_with_ratings['Book-Title'])
+
+    X_other = books_with_ratings.drop(columns=['Book-Title', 'Avg-Rating'])
+    X = pd.concat([pd.DataFrame(X_title.toarray()), X_other.reset_index(drop=True)], axis=1)
+    X.columns = X.columns.astype(str)
+    y = books_with_ratings['Avg-Rating']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    scaler = StandardScaler()
+    numerical_columns = ['Year-Of-Publication', 'Book-Author', 'Publisher']  
+    X_train[numerical_columns] = scaler.fit_transform(X_train[numerical_columns])
+    X_test[numerical_columns] = scaler.transform(X_test[numerical_columns])
+    print(X_train)
+
+    linreg = SGDRegressor(max_iter=1000, tol=1e-3, alpha=0.001, random_state=42)
+    linreg.fit(X_train, y_train)
+
+    y_pred = linreg.predict(X_test)
+    print("Mean Absolute Error:", mean_absolute_error(y_test, y_pred))
+
+    with open("d:/Университет/pyad-2024/lab2/linreg.pkl", "wb") as file:
         pickle.dump(linreg, file)
+
+books = pd.read_csv('d:/Университет/pyad-2024/lab2/Books.csv')
+ratings = pd.read_csv('d:/Университет/pyad-2024/lab2/Ratings.csv')
+modeling(books, ratings)
--- lab2/svd_rec.py
+++ lab2/svd_rec.py
@@ -1,17 +1,27 @@
 import pandas as pd
 import pickle
-
 from surprise import SVD
 from surprise import Dataset, Reader
 from surprise.model_selection import train_test_split
+from surprise.model_selection import GridSearchCV
 from surprise import accuracy
 
-
 def ratings_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
-    """Функция для предобработки таблицы Ratings.scv"""
+    """Функция для предобработки таблицы Ratings.csv"""
+    df = df[df['Book-Rating'] > 0]
 
-    pass
+    ISBN_count_ratings = df['ISBN'].value_counts().reset_index()
+    ISBN_count_ratings.columns = ['ISBN', 'count']
+    ISBN_drop = list(ISBN_count_ratings[ISBN_count_ratings['count'] <= 1]['ISBN'])
 
+    User_ID_count_ratings = df['User-ID'].value_counts().reset_index()
+    User_ID_count_ratings.columns = ['User-ID', 'count']
+    User_ID_drop = list(User_ID_count_ratings[User_ID_count_ratings['count'] <= 1]['User-ID'])
+
+    df = df[~df['User-ID'].isin(User_ID_drop)]
+    df = df[~df['ISBN'].isin(ISBN_drop)]
+
+    return df
 
 def modeling(ratings: pd.DataFrame) -> None:
     """В этой функции нужно выполнить следующие шаги:
@@ -19,9 +29,39 @@ def modeling(ratings: pd.DataFrame) -> None:
     2. Обучить и протестировать SVD
     3. Подобрать гиперпараметры (при необходимости)
     4. Сохранить модель"""
+    
+    ratings.rename(columns={'User-ID': 'user_id', 'ISBN': 'item_id', 'Book-Rating': 'rating'}, inplace=True)
 
-    # ...
-    svd = SVD()
-    # ...
+    reader = Reader(rating_scale=(1, 10))
+
+    data = Dataset.load_from_df(ratings[['user_id', 'item_id', 'rating']], reader)
+
+    trainset, testset = train_test_split(data, test_size=0.2)
+
+    param_grid = {
+    'n_factors': [ 30, 40],
+    'n_epochs': [20, 30],
+    'lr_all': [0.0025, 0.005],
+    'reg_all': [0.01, 0.02]
+    }
+    gs = GridSearchCV(SVD, param_grid, measures=['mae'], cv=3)
+    gs.fit(data)
+
+    print(f"Лучшие параметры: {gs.best_params['mae']}")
+    print(f"Лучший MAE: {gs.best_score['mae']}")
+
+    svd = gs.best_estimator['mae']
+    svd.fit(trainset)
+
+    predictions = svd.test(testset)
+    mae = accuracy.mae(predictions)
+
+    print(f'MAE: {mae}')
+    
     with open("svd.pkl", "wb") as file:
         pickle.dump(svd, file)
+
+df = pd.read_csv('Ratings.csv')
+
+df = ratings_preprocessing(df)
+modeling(df)
